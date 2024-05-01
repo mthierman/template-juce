a69f3240eee4a86190fcf327d85e5df2dbe0a3b1
mthierman <mthierman@gmail.com>
2024-05-01 13:40:06 -0600

    Testing checkpoint

--- src/Editor.cxx
+++ src/Editor.cxx
@@ -14,9 +14,11 @@ Editor::Editor(Processor& processor)
     juce::ignoreUnused(m_processor);
     addAndMakeVisible(m_browser);
 
-#if defined(HOT_RELOAD)
-    m_browser.goToURL("http://localhost:5173/");
-#endif
+    // #if defined(HOT_RELOAD)
+    //     m_browser.goToURL("http://localhost:5173/");
+    // #endif
+
+    m_browser.goToURL(juce::WebBrowserComponent::getResourceProviderRoot());
 
     setResizable(true, true);
     setResizeLimits(400, 300, 800, 600);
@@ -35,21 +37,33 @@ auto Editor::resized() -> void { m_browser.setBounds(getLocalBounds()); }
 auto Editor::getResource(const juce::String& url)
     -> std::optional<juce::WebBrowserComponent::Resource>
 {
+    DBG("TEST");
+
     const auto urlToRetrieve =
         url == "/" ? juce::String{"index.html"} : url.fromFirstOccurrenceOf("/", false, false);
 
-    std::vector<Resource> resources;
+    // std::vector<Resource> resources;
 
-    resources.emplace_back(Resource("/", "index_html"));
-    resources.emplace_back(Resource("/assets/index.css", "index_css"));
-    resources.emplace_back(Resource("/assets/index.js", "index_js"));
-    resources.emplace_back(Resource("/logo_dark.png", "logo_dark_png"));
-    resources.emplace_back(Resource("/logo_light.png", "logo_light_png"));
-    resources.emplace_back(Resource("/favicon.ico", "favicon_ico"));
+    // resources.emplace_back(Resource("/index.html", "index_html"));
+    // resources.emplace_back(Resource("/assets/index.css", "index_css"));
+    // resources.emplace_back(Resource("/assets/index.js", "index_js"));
+    // resources.emplace_back(Resource("/logo_dark.png", "logo_dark_png"));
+    // resources.emplace_back(Resource("/logo_light.png", "logo_light_png"));
+    // resources.emplace_back(Resource("/favicon.ico", "favicon_ico"));
 
-    for (const auto& res : resources)
+    // for (const auto& res : resources)
+    // {
+    //     if (urlToRetrieve.compare(res.m_path))
+    //     {
+    //         DBG(res.m_path);
+    //         return res.m_resource;
+    //     }
+    // }
+
+    if (urlToRetrieve == "index.html")
     {
-        if (urlToRetrieve.compare(res.m_path)) { return res.m_resource; }
+        DBG("index.html");
+        return Resource("index.html", "index_html").m_resource;
     }
 
     return std::nullopt;
