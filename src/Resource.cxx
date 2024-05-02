c66759339615385db65b19b6c24384382682640a
mthierman <mthierman@gmail.com>
2024-05-02 00:07:39 -0600

    Fix clang-format

--- src/Resource.cxx
+++ src/Resource.cxx
@@ -16,44 +16,27 @@ Resource::Resource(const juce::String& resourceName)
     mimeType = getMimeType(BinaryData::getNamedResourceOriginalFilename(resourceName.toUTF8()));
 }
 
-auto Resource::getMimeType(const juce::String& filename,
-                           const juce::String& defaultMimeType) -> juce::String
+auto Resource::getMimeType(const juce::String& filename, const juce::String& defaultMimeType)
+    -> juce::String
 {
     // https://developer.mozilla.org/en-US/docs/Web/HTTP/Basics_of_HTTP/MIME_types/Common_types
 
-    std::unordered_map<juce::String, juce::String> mimeTypes = {{"txt", "text/plain"},
-                                                                {"htm", "text/html"},
-                                                                {"html", "text/html"},
-                                                                {"css", "text/css"},
-                                                                {"js", "text/javascript"},
-                                                                {"mjs", "text/javascript"},
-                                                                {"csv", "text/csv"},
-                                                                {"json", "application/json"},
-                                                                {"otf", "font/otf"},
-                                                                {"ttf", "font/ttf"},
-                                                                {"woff", "font/woff"},
-                                                                {"woff2", "font/woff2"},
-                                                                {"ico", "image/vnd.microsoft.icon"},
-                                                                {"jpg", "image/jpeg"},
-                                                                {"jpeg", "image/jpeg"},
-                                                                {"gif", "image/gif"},
-                                                                {"png", "image/png"},
-                                                                {"webp", "image/webp"},
-                                                                {"tif", "image/tiff"},
-                                                                {"tiff", "image/tiff"},
-                                                                {"svg", "image/svg+xml"},
-                                                                {"bmp", "image/bmp"},
-                                                                {"mid", "audio/midi"},
-                                                                {"midi", "audio/midi"},
-                                                                {"aac", "audio/aac"},
-                                                                {"wav", "audio/wav"},
-                                                                {"weba", "audio/webm"},
-                                                                {"webm", "video/webm"},
-                                                                {"mp4", "video/mp4"},
-                                                                {"mpeg", "video/mpeg"},
-                                                                {"pdf", "application/pdf"},
-                                                                {"rtf", "application/rtf"},
-                                                                {"zip", "application/zip"}};
+    std::unordered_map<juce::String, juce::String> mimeTypes = {
+        {"txt", "text/plain"},      {"md", "text/markdown"},    {"htm", "text/html"},
+        {"html", "text/html"},      {"css", "text/css"},        {"js", "text/javascript"},
+        {"mjs", "text/javascript"}, {"csv", "text/csv"},        {"json", "application/json"},
+        {"otf", "font/otf"},        {"ttf", "font/ttf"},        {"woff", "font/woff"},
+        {"woff2", "font/woff2"},    {"avif", "image/avif"},     {"ico", "image/vnd.microsoft.icon"},
+        {"jpg", "image/jpeg"},      {"jpeg", "image/jpeg"},     {"gif", "image/gif"},
+        {"png", "image/png"},       {"webp", "image/webp"},     {"tif", "image/tiff"},
+        {"tiff", "image/tiff"},     {"svg", "image/svg+xml"},   {"svgz", "image/svg+xml"},
+        {"bmp", "image/bmp"},       {"mid", "audio/midi"},      {"midi", "audio/midi"},
+        {"aac", "audio/aac"},       {"aif", "audio/aiff"},      {"aiff", "audio/aiff"},
+        {"wav", "audio/wav"},       {"ogg", "audio/ogg"},       {"flac", "audio/flac"},
+        {"mp3", "audio/mpeg"},      {"weba", "audio/webm"},     {"webm", "video/webm"},
+        {"mp4", "video/mp4"},       {"mpeg", "video/mpeg"},     {"wasm", "application/wasm"},
+        {"pdf", "application/pdf"}, {"rtf", "application/rtf"}, {"xml", "application/xml"},
+        {"zip", "application/zip"}};
 
     if (auto it = mimeTypes.find(
             std::filesystem::path(filename.toStdString()).extension().string().substr(1));
