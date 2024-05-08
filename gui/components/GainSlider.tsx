d51bb2ad1d6c835b3c7e4800d33b9cc743f6220c
mthierman <mthierman@gmail.com>
2024-05-08 10:21:09 -0600

    Correctly type events

--- gui/components/GainSlider.tsx
+++ gui/components/GainSlider.tsx
@@ -1,53 +1,68 @@
 import InvertPhaseToggle from "components/InvertPhaseToggle";
 import * as Juce from "juce-framework-frontend";
-import { SyntheticEvent, useEffect, useRef, useState } from "react";
+import { ChangeEvent, useEffect, useRef, useState, WheelEvent } from "react";
+
+const clamp = () => {
+    return Math.max();
+};
 
 export default function GainSlider() {
-    const gainSlider = useRef<HTMLInputElement | null>(null);
-    const [gain, setGain] = useState("0.50");
-
-    const gainState = Juce.getSliderState("gain");
+    const slider = useRef<HTMLInputElement | null>(null);
+    const sliderState = Juce.getSliderState("gain");
+    const [value, setValue] = useState(sliderState.getNormalisedValue());
+    const [properties, setProperties] = useState(sliderState.properties);
 
     useEffect(() => {
-        gainState.valueChangedEvent.addListener(() => {
-            setGain(gainState.getNormalisedValue());
+        const valueListenerId = sliderState.valueChangedEvent.addListener(() => {
+            setValue(sliderState.getNormalisedValue());
         });
-        console.log(gainState);
+
+        return () => {
+            sliderState.valueChangedEvent.removeListener(valueListenerId);
+        };
     }, []);
 
-    const handleGainChange = (e: SyntheticEvent) => {
-        const target = e.target as HTMLInputElement;
-        setGain(target.value);
-        gainState.setNormalisedValue(target.value);
+    const handleChange = (event: ChangeEvent<HTMLInputElement>) => {
+        sliderState.setNormalisedValue(event.target.value);
+        setValue(event.target.value);
     };
 
-    const handleGainWheel = (e: SyntheticEvent) => {
-        const event = e.nativeEvent as WheelEvent;
+    const handleWheel = (event: WheelEvent<HTMLInputElement>) => {
+        let newValue: number;
 
         if (event.deltaY < 0) {
-            setGain((Number(gain) + (event.shiftKey ? 0.01 : 0.1)).toFixed(2));
+            newValue = Number(value) + (event.shiftKey ? 0.01 : 0.1);
         } else {
-            setGain((Number(gain) - (event.shiftKey ? 0.01 : 0.1)).toFixed(2));
+            newValue = Number(value) - (event.shiftKey ? 0.01 : 0.1);
         }
+
+        if (newValue < 0) {
+            newValue = 0;
+        } else if (newValue > 1) {
+            newValue = 1;
+        }
+
+        setValue(newValue.toFixed(2));
+        sliderState.setNormalisedValue(newValue.toFixed(2));
     };
 
-    useEffect(() => {
-        console.log(gain);
-    }, [gain]);
+    // useEffect(() => {
+    //     console.log(gain);
+    // }, [gain]);
 
     return (
         <div className="flex gap-1 p-2">
             <InvertPhaseToggle />
             <input
-                className="min-w-12 flex-grow cursor-pointer appearance-none bg-transparent focus:outline-none [&::-webkit-slider-runnable-track]:h-6 [&::-webkit-slider-runnable-track]:rounded-xl [&::-webkit-slider-runnable-track]:bg-black [&::-webkit-slider-runnable-track]:p-1 dark:[&::-webkit-slider-runnable-track]:bg-white [&::-webkit-slider-thumb]:h-4 [&::-webkit-slider-thumb]:w-4 [&::-webkit-slider-thumb]:appearance-none [&::-webkit-slider-thumb]:rounded-full [&::-webkit-slider-thumb]:bg-white hover:[&::-webkit-slider-thumb]:bg-emerald-400 dark:[&::-webkit-slider-thumb]:bg-black hover:dark:[&::-webkit-slider-thumb]:bg-emerald-600"
-                ref={gainSlider}
                 type="range"
+                ref={slider}
+                value={value}
                 min="0.00"
                 max="1.00"
-                value={gain}
-                onChange={handleGainChange}
-                onWheel={handleGainWheel}
                 step="0.01"
+                onChange={handleChange}
+                onWheel={handleWheel}
+                className="min-w-12 flex-grow cursor-pointer appearance-none bg-transparent focus:outline-none [&::-webkit-slider-runnable-track]:h-6 [&::-webkit-slider-runnable-track]:rounded-xl [&::-webkit-slider-runnable-track]:bg-black [&::-webkit-slider-runnable-track]:p-1 dark:[&::-webkit-slider-runnable-track]:bg-white [&::-webkit-slider-thumb]:h-4 [&::-webkit-slider-thumb]:w-4 [&::-webkit-slider-thumb]:appearance-none [&::-webkit-slider-thumb]:rounded-full [&::-webkit-slider-thumb]:bg-white hover:[&::-webkit-slider-thumb]:bg-emerald-400 dark:[&::-webkit-slider-thumb]:bg-black hover:dark:[&::-webkit-slider-thumb]:bg-emerald-600"
             />
             <span className="pointer-events-none text-nowrap">-20.0 dB</span>
         </div>
