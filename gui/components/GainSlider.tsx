5d9c942e4d8ba18e6f7f60710092cad10e2f5d54
mthierman <mthierman@gmail.com>
2024-05-07 21:50:24 -0600

    Finish removing @apply

--- gui/components/GainSlider.tsx
+++ gui/components/GainSlider.tsx
@@ -44,7 +44,12 @@ export default function GainSlider() {
         <label className="select-none">
             -20.0 dB
             <input
-                className="flex-grow cursor-pointer appearance-none bg-transparent focus:outline-none [&::-webkit-slider-runnable-track]:bg-black dark:[&::-webkit-slider-runnable-track]:bg-white [&::-webkit-slider-thumb]:bg-white hover:[&::-webkit-slider-thumb]:bg-blue-500 dark:[&::-webkit-slider-thumb]:bg-black hover:dark:[&::-webkit-slider-thumb]:bg-blue-500"
+                className="flex-grow cursor-pointer appearance-none bg-transparent focus:outline-none [&::-webkit-slider-runnable-track]:h-6 [&::-webkit-slider-runnable-track]:rounded-xl [&::-webkit-slider-runnable-track]:bg-black [&::-webkit-slider-runnable-track]:p-1 dark:[&::-webkit-slider-runnable-track]:bg-white [&::-webkit-slider-thumb]:h-4
+                [&::-webkit-slider-thumb]:w-4 [&::-webkit-slider-thumb]:appearance-none [&::-webkit-slider-thumb]:rounded-full
+                [&::-webkit-slider-thumb]:bg-white
+                hover:[&::-webkit-slider-thumb]:bg-blue-500
+                dark:[&::-webkit-slider-thumb]:bg-black
+                hover:dark:[&::-webkit-slider-thumb]:bg-blue-500"
                 ref={gainSlider}
                 type="range"
                 min="0.00"
