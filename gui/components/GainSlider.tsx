61d5dc276d8fc5290da27f6500c314f19d3a9b68
mthierman <mthierman@gmail.com>
2024-05-08 10:34:13 -0600

    Better events for slider

--- gui/components/GainSlider.tsx
+++ gui/components/GainSlider.tsx
@@ -1,6 +1,6 @@
 import InvertPhaseToggle from "components/InvertPhaseToggle";
 import * as Juce from "juce-framework-frontend";
-import { ChangeEvent, useEffect, useRef, useState, WheelEvent } from "react";
+import { ChangeEvent, MouseEvent, WheelEvent, useEffect, useRef, useState } from "react";
 
 const clamp = () => {
     return Math.max();
@@ -23,8 +23,17 @@ export default function GainSlider() {
     }, []);
 
     const handleChange = (event: ChangeEvent<HTMLInputElement>) => {
-        sliderState.setNormalisedValue(event.target.value);
-        setValue(event.target.value);
+        sliderState.setNormalisedValue(event.currentTarget.value);
+        setValue(event.currentTarget.value);
+    };
+
+    const handleMouseDown = () => {
+        sliderState.sliderDragStarted();
+    };
+
+    const handleMouseUp = (event: MouseEvent<HTMLInputElement>) => {
+        sliderState.setNormalisedValue(event.currentTarget.value);
+        sliderState.sliderDragEnded();
     };
 
     const handleWheel = (event: WheelEvent<HTMLInputElement>) => {
@@ -62,6 +71,8 @@ export default function GainSlider() {
                 step="0.01"
                 onChange={handleChange}
                 onWheel={handleWheel}
+                onMouseDown={handleMouseDown}
+                onMouseUp={handleMouseUp}
                 className="min-w-12 flex-grow cursor-pointer appearance-none bg-transparent focus:outline-none [&::-webkit-slider-runnable-track]:h-6 [&::-webkit-slider-runnable-track]:rounded-xl [&::-webkit-slider-runnable-track]:bg-black [&::-webkit-slider-runnable-track]:p-1 dark:[&::-webkit-slider-runnable-track]:bg-white [&::-webkit-slider-thumb]:h-4 [&::-webkit-slider-thumb]:w-4 [&::-webkit-slider-thumb]:appearance-none [&::-webkit-slider-thumb]:rounded-full [&::-webkit-slider-thumb]:bg-white hover:[&::-webkit-slider-thumb]:bg-emerald-400 dark:[&::-webkit-slider-thumb]:bg-black hover:dark:[&::-webkit-slider-thumb]:bg-emerald-600"
             />
             <span className="pointer-events-none text-nowrap">-20.0 dB</span>
