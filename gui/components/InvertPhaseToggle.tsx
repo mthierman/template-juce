0d76711e845c677e604b73294bda551100c09a1e
mthierman <mthierman@gmail.com>
2024-05-07 23:26:35 -0600

    Get checkbox working again

--- gui/components/InvertPhaseToggle.tsx
+++ gui/components/InvertPhaseToggle.tsx
@@ -1,27 +1,19 @@
 import { SyntheticEvent, useEffect, useRef, useState } from "react";
 import * as Juce from "juce-framework-frontend";
-import { svgUrl } from "modules/theme";
-import phase_toggle from "images/phase_toggle.svg?raw";
-
-const phaseToggleSvg = {
-    default: svgUrl(phase_toggle),
-};
 
 export default function InvertPhaseToggle() {
     const invertPhaseToggle = useRef<HTMLInputElement | null>(null);
     const [phase, setPhase] = useState(false);
+
     const svgRef = useRef<SVGSVGElement | null>(null);
     const svgRectRef = useRef<SVGRectElement | null>(null);
+    const svgCircleRef = useRef<SVGCircleElement | null>(null);
 
     // https://www.geeksforgeeks.org/how-to-change-svg-icon-color-on-click-in-javascript/
     // svgRef.current?.setAttribute("fill", "red");
     // svgRectRef.current?.setAttribute("fill", "red");
 
     const phaseState = Juce.getToggleState("invertPhase");
-
-    useEffect(() => {
-        console.log(phaseState);
-    }, []);
 
     const handleInvertPhaseChange = (e: SyntheticEvent) => {
         const target = e.target as HTMLInputElement;
@@ -33,7 +25,7 @@ export default function InvertPhaseToggle() {
     }, [phase]);
 
     return (
-        <div className="flex bg-red-400">
+        <label className="flex">
             <input
                 className="appearance-none"
                 ref={invertPhaseToggle}
@@ -50,24 +42,26 @@ export default function InvertPhaseToggle() {
                 fill="none"
                 xmlns="http://www.w3.org/2000/svg">
                 <rect
-                    className="fill-black group-hover:fill-emerald-400 dark:group-hover:fill-emerald-600"
                     ref={svgRectRef}
+                    className="group-hover:fill-emerald-400 dark:group-hover:fill-emerald-600"
                     x="186.056"
                     y="17"
                     width="24"
                     height="275"
                     rx="8"
                     transform="rotate(20 186.056 17)"
-                    fill="black"
+                    fill={phase ? "#FF0000" : "#000"}
                 />
                 <circle
-                    className="stroke-black group-hover:stroke-emerald-400 dark:group-hover:stroke-emerald-600"
+                    ref={svgCircleRef}
+                    className="group-hover:stroke-emerald-400 dark:group-hover:stroke-emerald-600"
                     cx="150"
                     cy="150"
                     r="88"
+                    stroke={phase ? "#FF0000" : "#000"}
                     strokeWidth="24"
                 />
             </svg>
-        </div>
+        </label>
     );
 }
