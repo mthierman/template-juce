0eee0d78bada830fdb7d49800a4b3544f4b4376f
mthierman <mthierman@gmail.com>
2024-05-08 12:36:25 -0600

    Improve JS for phase toggle

--- gui/components/InvertPhaseToggle.tsx
+++ gui/components/InvertPhaseToggle.tsx
@@ -1,42 +1,57 @@
 import * as Juce from "juce-framework-frontend";
-import { SyntheticEvent, useEffect, useRef, useState } from "react";
+import { ChangeEvent, useEffect, useRef, useState } from "react";
 import tailwindConfig from "root/tailwind.config";
 import resolveConfig from "tailwindcss/resolveConfig.js";
 
 const tailwind = resolveConfig(tailwindConfig).theme.colors;
 
 export default function InvertPhaseToggle() {
-    const phaseToggle = useRef<HTMLInputElement | null>(null);
-    const [phase, setPhase] = useState(false);
+    const toggle = useRef<HTMLInputElement | null>(null);
+    const toggleState = Juce.getToggleState("invertPhase");
 
-    const phaseState = Juce.getToggleState("invertPhase");
+    const [checked, setChecked] = useState(toggleState.getValue());
+    const [properties, setProperties] = useState(toggleState.properties);
 
-    const handlePhaseChange = (e: SyntheticEvent) => {
-        const target = e.target as HTMLInputElement;
-        setPhase(target.checked);
+    useEffect(() => {
+        const valueListenerId = toggleState.valueChangedEvent.addListener(() => {
+            setChecked(toggleState.getValue());
+        });
+        const propertiesListenerId = toggleState.propertiesChangedEvent.addListener(() => {
+            setProperties(toggleState.properties);
+        });
+
+        return () => {
+            toggleState.valueChangedEvent.removeListener(valueListenerId);
+            toggleState.propertiesChangedEvent.removeListener(propertiesListenerId);
+        };
+    }, []);
+
+    const handleChange = (event: ChangeEvent<HTMLInputElement>) => {
+        toggleState.setValue(event.currentTarget.checked);
+        setChecked(event.currentTarget.checked);
     };
 
     useEffect(() => {
-        console.log(phase);
-    }, [phase]);
+        console.log(checked);
+    }, [checked]);
 
     return (
         <label className="flex items-center">
             <input
                 className="appearance-none"
-                ref={phaseToggle}
+                ref={toggle}
                 type="checkbox"
-                checked={phase}
-                onChange={handlePhaseChange}
+                checked={checked}
+                onChange={handleChange}
             />
 
             <svg
-                className={`w-6 cursor-pointer rounded-full shadow-lg shadow-black outline-dotted outline-2 outline-transparent hover:outline-emerald-400 ${phase ? "bg-blue-900" : "bg-white"}`}
+                className={`w-6 cursor-pointer rounded-full shadow-lg shadow-black outline-dotted outline-2 outline-transparent hover:outline-emerald-400 ${checked ? "bg-blue-900" : "bg-white"}`}
                 viewBox="0 0 300 300"
                 fill="none"
                 xmlns="http://www.w3.org/2000/svg">
                 <rect
-                    className={`${phase ? "fill-blue-400" : "fill-black"}`}
+                    className={`${checked ? "fill-blue-400" : "fill-black"}`}
                     x="186.056"
                     y="17"
                     width="24"
@@ -46,7 +61,7 @@ export default function InvertPhaseToggle() {
                     fill="black"
                 />
                 <circle
-                    className={`${phase ? "stroke-blue-400" : "stroke-black"}`}
+                    className={`${checked ? "stroke-blue-400" : "stroke-black"}`}
                     cx="150"
                     cy="150"
                     r="88"
