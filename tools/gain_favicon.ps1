01c9594df8fbec0930fcfaa5731627dc62c3c15c
mthierman <mthierman@gmail.com>
2024-05-09 11:42:25 -0600

    Gain favicon script update

--- tools/gain_favicon.ps1
+++ tools/gain_favicon.ps1
@@ -7,4 +7,4 @@ function ConvertTo-Ico
     magick convert -background none $in -define icon:auto-resize="256,128,96,80,72,64,60,48,40,36,32,30,24,20,16" $out
 }
 
-ConvertTo-Ico icon.png favicon.ico
+ConvertTo-Ico projects/plugins/gain/data/icon.png projects/plugins/gain/public/favicon.ico
