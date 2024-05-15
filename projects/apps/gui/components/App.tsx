ddedfe56f6d213a543f58550f1050eba1cafe717
mthierman <mthierman@gmail.com>
2024-05-15 14:50:11 -0600

    Color grabber

--- projects/apps/gui/components/App.tsx
+++ projects/apps/gui/components/App.tsx
@@ -1,5 +1,5 @@
 import "css/index.css";
-import { useEffect, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 
 const tracks = [
     "Prelude to Stranded",
@@ -14,19 +14,42 @@ const tracks = [
 ];
 
 export default function App() {
+    const cover = useRef<HTMLDivElement | null>(null);
+    const coverImage = useRef<HTMLImageElement | null>(null);
+
+    useEffect(() => {
+        const img = coverImage.current;
+        img.onload = () => {
+            console.log(img.src);
+            const ctx = document.createElement("canvas").getContext("2d");
+            ctx.imageSmoothingEnabled = true;
+            ctx.drawImage(img, 0, 0, 4, 4);
+            const i = ctx.getImageData(0, 0, 1, 1).data;
+            console.log(i);
+            const rgba = `rgba(${i[0]},${i[1]},${i[2]},${i[3]})`;
+            const HEX = "#" + ((1 << 24) + (i[0] << 16) + (i[1] << 8) + i[2]).toString(16).slice(1);
+            cover.current.style.backgroundColor = HEX;
+        };
+    }, [coverImage.current]);
+
     return (
         <>
             <div id="tracks">
                 <ol id="tracklist">
                     {tracks.map((track, index) => (
-                        <li className="track">{track}</li>
+                        <li key={index} className="track">
+                            {track}
+                        </li>
                     ))}
                 </ol>
             </div>
-            <div id="cover">
+            <div id="cover" ref={cover}>
                 <img
+                    ref={coverImage}
                     id="cover-image"
-                    src="https://f4.bcbits.com/img/a2157581263_10.jpg"
+                    // src="/a1101171123_10.jpg"
+                    // src="/a2157581263_10.jpg"
+                    src="/funhouse_50.jpg"
                     draggable={false}
                 />
             </div>
