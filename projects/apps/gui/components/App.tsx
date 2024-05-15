b01e313604776cd37c9c3b4708081f5ec8170465
mthierman <mthierman@gmail.com>
2024-05-14 23:14:15 -0600

    Grid

--- projects/apps/gui/components/App.tsx
+++ projects/apps/gui/components/App.tsx
@@ -1,5 +1,4 @@
 import "css/index.css";
-import { applyTheme, loadTheme } from "modules/theme";
 import { useEffect, useState } from "react";
 
 const trackList = [
@@ -15,38 +14,18 @@ const trackList = [
 ];
 
 export default function App() {
-    const [theme, setTheme] = useState(loadTheme());
-
-    applyTheme(theme);
-
-    useEffect(() => {
-        const themeChange = () => {
-            if (theme === "system") {
-                applyTheme(theme);
-            }
-        };
-
-        const mql = window.matchMedia("(prefers-color-scheme: dark)");
-
-        mql.addEventListener("change", themeChange);
-
-        return () => {
-            mql.removeEventListener("change", themeChange);
-        };
-    }, []);
-
     return (
         <>
-            <ol className="min-w-48 overflow-y-auto overflow-x-hidden bg-yellow-400 p-4 leading-tight">
-                {trackList.map((track, index) => (
-                    <li className="">{`${index.toString().padStart(2, "0")}. ${track}`}</li>
-                ))}
-            </ol>
-            <img
-                className="aspect-square shadow-2xl m-auto max-w-96 shadow-black"
-                src="https://f4.bcbits.com/img/a2157581263_10.jpg"
-                draggable={false}
-            />
+            <div id="tracks">
+                <ol>
+                    {trackList.map((track, index) => (
+                        <li className="">{`${index.toString().padStart(2, "0")}. ${track}`}</li>
+                    ))}
+                </ol>
+            </div>
+            <div id="cover">
+                <img id="cover-image" src="https://f4.bcbits.com/img/a2157581263_10.jpg" draggable={false} />
+            </div>
         </>
     );
 }
