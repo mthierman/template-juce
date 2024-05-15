3d823c9525ebb0fd15bca37392400483cfa5a978
mthierman <mthierman@gmail.com>
2024-05-14 22:04:47 -0600

    Clear styling

--- projects/apps/gui/components/App.tsx
+++ projects/apps/gui/components/App.tsx
@@ -2,6 +2,18 @@ import "css/index.css";
 import { applyTheme, loadTheme } from "modules/theme";
 import { useEffect, useState } from "react";
 
+const trackList = [
+    "Prelude to Stranded",
+    "Fever Dream",
+    "Changes",
+    "Sun",
+    "Stranded",
+    "25 Notes",
+    "Island Time",
+    "Island Time Afterlude",
+    "Landed",
+];
+
 export default function App() {
     const [theme, setTheme] = useState(loadTheme());
 
@@ -24,15 +36,17 @@ export default function App() {
     }, []);
 
     return (
-        <div className="grid size-full grid-flow-col grid-cols-2">
-            <div className=""></div>
-            <div className="m-auto">
-                <img
-                    className="size-96 min-w-96 aspect-square shadow-2xl shadow-black"
-                    src="https://f4.bcbits.com/img/a2157581263_10.jpg"
-                    draggable={false}
-                />
-            </div>
-        </div>
+        <>
+            <ol className="bg-yellow-400 p-4 leading-tight">
+                {trackList.map((track, index) => (
+                    <li className="text-ellipsis text-nowrap">{`${index.toString().padStart(2, "0")}. ${track}`}</li>
+                ))}
+            </ol>
+            <img
+                className="aspect-square shadow-2xl shadow-black"
+                src="https://f4.bcbits.com/img/a2157581263_10.jpg"
+                draggable={false}
+            />
+        </>
     );
 }
