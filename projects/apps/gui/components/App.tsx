bfeaf748895779366ebd1c20f3f4caf1d6c48772
mthierman <mthierman@gmail.com>
2024-05-14 20:55:13 -0600

    Init

--- projects/apps/gui/components/App.tsx
+++ projects/apps/gui/components/App.tsx
@@ -23,5 +23,16 @@ export default function App() {
         };
     }, []);
 
-    return <></>;
+    return (
+        <div className="grid size-full grid-flow-col grid-cols-2">
+            <div className=""></div>
+            <div className="m-auto">
+                <img
+                    className="size-96 min-w-96 aspect-square shadow-2xl shadow-black"
+                    src="https://f4.bcbits.com/img/a2157581263_10.jpg"
+                    draggable={false}
+                />
+            </div>
+        </div>
+    );
 }
