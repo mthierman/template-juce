c275591eef37b93fc1cc67280d2b54e9c7bd7894
mthierman <mthierman@gmail.com>
2024-05-15 21:19:10 -0600

    2/3 album cover

--- projects/apps/gui/components/App.tsx
+++ projects/apps/gui/components/App.tsx
@@ -54,6 +54,12 @@ export default function App() {
     const cover = useRef<HTMLDivElement | null>(null);
     const coverImage = useColorFromImage();
 
+    useEffect(() => {
+        const brainz = () => {
+            fetch("https://musicbrainz.org/ws/2/").then((value) => console.log(value));
+        };
+    }, []);
+
     return (
         <>
             <div id="tracks">
