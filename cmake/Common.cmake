7030f60fd07c24313cf24098a69d001b7de98e30
mthierman <mthierman@gmail.com>
2024-06-12 11:57:11 -0600

    Use C++ 20

--- cmake/Common.cmake
+++ cmake/Common.cmake
@@ -1,32 +1,31 @@
 add_library(
     jwv_features
     INTERFACE
-)
+    )
 
 add_library(
     jwv::features
     ALIAS
     jwv_features
-)
+    )
 
 target_compile_features(
     jwv_features
     INTERFACE c_std_17
-              cxx_std_23
-)
+              cxx_std_20
+    )
 
 add_library(
     jwv_definitions
     INTERFACE
-)
+    )
 
 add_library(
     jwv::definitions
     ALIAS
     jwv_definitions
-)
+    )
 
 target_compile_definitions(
-    jwv_definitions
-    INTERFACE _SILENCE_CXX23_ALIGNED_STORAGE_DEPRECATION_WARNING
-)
+    jwv_definitions INTERFACE _SILENCE_CXX23_ALIGNED_STORAGE_DEPRECATION_WARNING
+    )
